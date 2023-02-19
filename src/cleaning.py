b3b477b84973babc6510f999c7c66b9a8b4a23ac
newracket <newracket@users.noreply.github.com>
2023-02-18 21:50:57 -0800

    Added functions to:
     Force everything into lowercase
     Filter out non alphabetic characters (and whitespace)
     Split string into words
     Filter by number of words

---
+++ src/cleaning.py
@@ -0,0 +1,74 @@
+import pandas as pd
+
+
+def filterByNumWords(df: pd.DataFrame, numWords: int) -> pd.DataFrame:
+    """
+    Filter out comments that have fewer words than numWords
+
+    :param df: dataframe
+    :param numWords: int
+    :return: dataframe with comments with fewer words than numWords filtered out
+    """
+
+    if "comment_text_words" not in df.columns:
+        df = splitIntoWords(df)
+
+    return df.loc[df["comment_text_words"].str.len() > numWords]
+
+
+def filterNonEnglishChars(df: pd.DataFrame) -> pd.DataFrame:
+    """
+        Filter out non-english characters
+
+        :param df: dataframe
+        :return: dataframe with non-english characters filtered out
+    """
+
+    # df["comment_text"] = df['comment_text'].str \
+    #     .encode('ascii', 'ignore').str.decode('ascii')
+    df["comment_text"].replace(r"[^A-Za-z\s]+", "", regex=True,
+                               inplace=True)
+
+    return df
+
+
+def toLowerCase(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Forces all characters to be lowercase
+
+    :param df: dataframe
+    :return: dataframe with all characters forced to be lowercase
+    """
+
+    df["comment_text"] = df["comment_text"].str.lower()
+
+    return df
+
+
+def splitIntoWords(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Split comments into words and forces them to be lowercase
+
+    :param df: dataframe
+    :return: dataframe with comments split into words
+    """
+
+    df["comment_text_words"] = df["comment_text"].str.split("\\s+")
+
+    return df
+
+
+def isEnglish(s: str) -> bool:
+    """
+    Check if a string contains all english characters
+
+    :param s: string to check
+    :return: whether string contains only english characters
+    """
+
+    try:
+        s.encode(encoding='utf-8').decode('ascii')
+    except UnicodeDecodeError:
+        return False
+    else:
+        return True
--- src/main.py
+++ src/main.py
@@ -1,60 +1,17 @@
 import pandas as pd
 
+from cleaning import filterNonEnglishChars, filterByNumWords, splitIntoWords, \
+    toLowerCase
+
 
 def main():
     df = pd.read_csv("../input/train.csv")
     print(df)
+    df = toLowerCase(df)
     df = filterNonEnglishChars(df)
-    df = filterLength(df, 10)
+    df = splitIntoWords(df)
+    df = filterByNumWords(df, 3)
     print(df)
-
-
-def isEnglish(s: str) -> bool:
-    """
-    Check if a string contains all english characters
-
-    :param s: string to check
-    :return: whether string contains only english characters
-    """
-    try:
-        s.encode(encoding='utf-8').decode('ascii')
-    except UnicodeDecodeError:
-        return False
-    else:
-        return True
-
-
-def filterLength(df: pd.DataFrame, length: int) -> pd.DataFrame:
-    """
-    Filter out comments that are less than a certain length
-
-    :param df: dataframe
-    :param length: int
-    :return: dataframe with comments that are greater than the length
-    """
-
-    df["len"] = df["comment_text"].str.len()
-    # Could also check if all toxic columns are 0
-    # print(df.loc[(df["len"] <= length) & (df["toxic"] == 0) & (
-    #         df["severe_toxic"] == 0) & (df["obscene"] == 0) & (
-    #                      df["threat"] == 0) & (df["insult"] == 0) & (
-    #                      df["identity_hate"] == 0)])
-
-    return df.loc[df["len"] > length]
-
-
-def filterNonEnglishChars(df: pd.DataFrame) -> pd.DataFrame:
-    """
-        Filter out non-english characters
-
-        :param df: dataframe
-        :return: dataframe with non-english characters filtered out
-    """
-
-    df["comment_text"] = df['comment_text'].str \
-        .encode('ascii', 'ignore').str.decode('ascii')
-
-    return df
 
 
 main()
